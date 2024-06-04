41f73d9ad7d87fc91c9b90703557a27b47bcc4d0
Puja <154424517+pk8184732@users.noreply.github.com>
2024-06-04 14:07:52 +0530

    ok

---
+++ book_ui_api/android/app/src/main/kotlin/com/example/book_ui_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.book_ui_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ book_ui_api/lib/splash_screen/book_splash.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+class BookSplash extends StatefulWidget {
+  const BookSplash({super.key});
+
+  @override
+  State<BookSplash> createState() => _BookSplashState();
+}
+
+class _BookSplashState extends State<BookSplash> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+
+      ),
+    );
+  }
+}
